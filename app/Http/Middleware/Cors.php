260099b7568045ec50e196eee5caa4ac0b8bfdcf
ryuuho <krjima@yahoo.co.jp>
2021-12-12 12:17:43 +0900

    cors_middleware6

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -16,8 +16,8 @@ class Cors
     public function handle($request, Closure $next)
     {
         return $next($request)
-            ->header('Access-Control-Allow-Origin', env('CLIENT_BASE_URL'))
-            ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
-            ->header('Access-Control-Allow-Headers', 'Content-Type');
+            ->header('Access-Control-Allow-Origin', env('CLIENT_BASE_URL'));
+            // ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+            // ->header('Access-Control-Allow-Headers', 'Content-Type');
     }
 }
