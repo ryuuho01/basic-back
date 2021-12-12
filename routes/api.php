fd8a458bbcd551266d5247e284ee46bd1bb169a0
ryuuho <krjima@yahoo.co.jp>
2021-12-12 12:13:13 +0900

    cors_middleware4

--- routes/api.php
+++ routes/api.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\GenreController;
 use App\Http\Controllers\ShopController;
 use App\Http\Controllers\FavoriteController;
 use App\Http\Controllers\ReservationController;
+use App\Http\Middleware\Cors;
 
 Route::apiResource('/user', UserController::class);
 Route::apiResource('/area', AreaController::class);
@@ -15,13 +16,13 @@ Route::apiResource('/shop', ShopController::class);
 Route::apiResource('/favorite', FavoriteController::class);
 Route::apiResource('/reservation', ReservationController::class);
 
-Route::middleware(['cors'])->group(function () {
+// Route::middleware(['cors'])->group(function () {
 
-  Route::apiResource('/user', UserController::class);
-  Route::apiResource('/area', AreaController::class);
-  Route::apiResource('/genre', GenreController::class);
-  Route::apiResource('/shop', ShopController::class);
-  Route::apiResource('/favorite', FavoriteController::class);
-  Route::apiResource('/reservation', ReservationController::class);
+//   Route::apiResource('/user', UserController::class);
+//   Route::apiResource('/area', AreaController::class);
+//   Route::apiResource('/genre', GenreController::class);
+//   Route::apiResource('/shop', ShopController::class);
+//   Route::apiResource('/favorite', FavoriteController::class);
+//   Route::apiResource('/reservation', ReservationController::class);
 
-});
+// });
